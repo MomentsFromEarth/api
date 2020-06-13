dd9936cc7930b30dcc989a6e7951895a3214f19e
Daniel Stepp <willstepp@gmail.com>
2020-06-13 12:31:28 -0400

    updated endpoint to test build process

--- internal/api.go
+++ internal/api.go
@@ -11,7 +11,7 @@ func Init() *gin.Engine {
 	r := gin.Default()
 	r.GET("/pingmetest", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pingmetest",
+			"message": "pongmetest",
 		})
 	})
 	return r
